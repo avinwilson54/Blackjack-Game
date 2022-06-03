88027d2d1d493ca9b2207cd7a798e7d638c93d19
Avin Wilson <avinwilson54@gmail.com>
2022-06-03 08:08:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let firstCard = 10 ;
+let secondCard = 11 ;
+
+let sum = firstCard + secondCard  ;
+
+console.log(sum); 
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂") ;
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳") ;
+} else {
+    console.log("You're out of the game! 😭")
+}
