490a539bc0b232d6cac260a51994e746f55ba26d
Avin Wilson <avinwilson54@gmail.com>
2022-06-04 05:34:22 +0530

    Partially working

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 
-let firstCard = 10 ;
+let firstCard = 1 ;
 let secondCard = 11 ;
+let hasBlackjack = false ;
+let isAlive = true ;
+let message = "" ;
+let messageEl = document.getElementById("message-el") ;
+let sumEl = document.querySelector("#sum-el") ; // querySelector s another powerful way of selecting html elements based on their classes or id's
 
 let sum = firstCard + secondCard  ;
 
-console.log(sum); 
-
-if (sum < 21) {
-    console.log("Do you want to draw a new card? 🙂") ;
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳") ;
-} else {
-    console.log("You're out of the game! 😭")
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? " ;
+        sumEl.textContent = "Sum: " + sum ;
+    } else if (sum === 21) {
+        message =  "You've got Blackjack! " ;
+        sumEl.textContent = "Sum: " + sum ;
+        hasBlackjack = true ;
+    } else {
+        message = "You're out of the game! " ;
+        sumEl.textContent = "Sum: " + sum ;
+        isAlive = false ;
+    }
+    messageEl.textContent = message ;
 }
+
+startGame() ;
